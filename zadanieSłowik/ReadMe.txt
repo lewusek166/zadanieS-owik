42e3d263ef0b056fd0db328d9ca9f248e0a66ee4
Przemek <lewusek166@gmail.com>
2017-10-23 22:25:57 +0200

    Add project files.

---
+++ zadanieSłowik/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : zadanieS這wik Project Overview
+========================================================================
+
+AppWizard has created this zadanieS這wik application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your zadanieS這wik application.
+
+
+zadanieS這wik.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+zadanieS這wik.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+zadanieS這wik.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named zadanieS這wik.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ zadanieSłowik/zadanieSłowik.cpp
@@ -0,0 +1,151 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+
+
+
+class Porównywanie 
+{
+public:
+
+	string * dok1;
+	string * dok1p;
+	string fraza;
+	int iterator[9];
+	int prawda;
+	bool potwierdzenie;
+	bool potwierdzenie1;
+	Porównywanie ();
+	~Porównywanie();
+	void Pobranie_fazy();
+	void odczyt();
+	void tablica_jednorodna();
+
+
+private:
+
+};
+
+
+
+
+Porównywanie ::Porównywanie ()
+{
+	potwierdzenie = false;
+	potwierdzenie1 = false;
+	
+	
+}
+
+Porównywanie::~Porównywanie()
+{
+	delete[] dok1;
+}
+
+void Porównywanie::Pobranie_fazy()
+{
+	cout << "Podaj wyrazy do porównania w dokumencie " << endl;
+	cin >> fraza;
+
+	cout << "Wyrazy do porównania" << fraza << endl;
+
+
+
+
+
+
+}
+
+void Porównywanie::odczyt()
+{
+	iterator[0] = 0;
+	fstream plik1;
+
+	plik1.open("dokument1.txt", ios::in);
+	if (plik1.good() == false) {
+		cout << "B³¹d otwarcia pliku 1" << endl;
+		exit(0);
+	}
+	string pomoc1;
+	while (getline(plik1, pomoc1, ' ')) {
+		iterator[0]++;
+	}
+	cout << "ilosc slow = " << iterator[0] << endl;
+	dok1 = new string[iterator[0]+1];
+	
+	plik1.close();
+	plik1.open("dokument1.txt", ios::in);
+	if (plik1.good() == false) {
+		cout << "B³¹d otwarcia pliku 1" << endl;
+		exit(0);
+	}
+	int i = 0;
+	while (getline(plik1, dok1[i], ' '))
+	{
+		cout << dok1[i] << endl;
+		i++;
+	}
+
+	
+
+
+plik1.close();
+
+
+
+}
+
+void Porównywanie::tablica_jednorodna()
+{
+	dok1p = new string[iterator[0] + 1];
+	int liczydlo = 1;
+	int i = 1;
+	dok1p[0] = dok1[0];
+	for (i; i <= iterator[0]; i++) {
+		prawda = 0;
+		for (int j = i-1; j >= 0; j--) {
+			
+			cout << dok1[i] << " :: " << dok1p[j] << endl;
+			if (dok1[i] != dok1p[j]) {
+				
+				prawda -= 1;
+			}
+			else
+			{
+				prawda += iterator[0];
+			}
+			
+				
+		}if (prawda<=0) {
+			cout << "zapis  :: " << dok1[i] << endl;
+			dok1p[liczydlo] = dok1[i];
+			liczydlo++;
+			
+		}
+		}
+	
+
+	for (int i = 0; i < iterator[0];i++) {
+		cout << dok1p[i] << endl;
+	}
+}
+
+
+
+
+int main()
+{
+	Porównywanie obiekt;
+	obiekt.odczyt();
+	obiekt.tablica_jednorodna();
+	
+	
+    return 0;
+	
+}
+
